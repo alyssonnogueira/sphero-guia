18d3a1c183a90ac206d469536e4ed94a9a1a11c6
Vitor Plentz <vrplentz@inf.ufpel.edu.br>
2016-10-15 15:32:17 -0300

    App conectando a Sphero; MainActivity liberando a Sphero apenas após estar conectado.

--- Sphero-Guia/app/src/main/java/com/pyramitec/sphero_guia/MainActivity.java
+++ Sphero-Guia/app/src/main/java/com/pyramitec/sphero_guia/MainActivity.java
@@ -10,53 +10,45 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 import com.orbotix.DualStackDiscoveryAgent;
 import com.orbotix.command.RollCommand;
 import com.orbotix.common.*;
 import com.orbotix.classic.*;
 import com.orbotix.*;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import static android.view.View.GONE;
 
+public class MainActivity extends AppCompatActivity {
     private ConvenienceRobot mRobot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "VAAAI SPHEEROOOOOOWWW!! :D ", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                mRobot.sendCommand( new RollCommand( 60, (float) 0.5, RollCommand.State.GO ) );
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        DualStackDiscoveryAgent.getInstance().addRobotStateListener( new RobotChangedStateListener(){
+        DiscoveryAgentClassic.getInstance().addRobotStateListener( new RobotChangedStateListener(){
             @Override
             public void handleRobotChangedState(Robot robot, RobotChangedStateNotificationType type){
                 switch (type){
                     case Online:
                         if (robot instanceof RobotClassic) {
                             mRobot = new Sphero(robot);
+                            TextView textViewConnection = (TextView) findViewById(R.id.textViewSpheroConnection);
+                            textViewConnection.setVisibility(GONE);
+                            Button buttonGo = (Button) findViewById(R.id.buttonGo);
+                            buttonGo.setVisibility(View.VISIBLE);
+                            buttonGo.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    Snackbar.make(view, "VAAAI SPHEEROOOOOOWWW!! :D ", Snackbar.LENGTH_LONG)
+                                            .setAction("Action", null).show();
+                                    if(mRobot!=null)
+                                        mRobot.sendCommand( new RollCommand( 60, (float) 0.5, RollCommand.State.GO ) );
+                                }
+                            });
+                            Toast.makeText(getApplicationContext(), "Connected", Toast.LENGTH_SHORT).show();
                         }
                         break;
                     case Disconnected:
@@ -64,64 +56,9 @@ public class MainActivity extends AppCompatActivity
                 }
             }
         });
+
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
 
     @Override
     protected void onStart() {
